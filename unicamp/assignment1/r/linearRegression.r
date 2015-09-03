07b21c91fadf0e3f29e19e304d7a47b48f703d41
Victor Leal <victorleal18@gmail.com>
2015-09-03 18:44:17 -0300

    Script R com graficos e organizacao das pastas

---
+++ unicamp/assignment1/r/linearRegression.r
@@ -0,0 +1,71 @@
+#rm(list=ls())
+#sink("log.txt")
+time1 <- proc.time()
+
+# Load the data
+#data <- read.csv(file.choose(), header=F)
+
+# Get the first 324600 (70%) examples for the 90 variables
+#X <- data[1:324600, c(seq(2,91,by=3))]
+
+# Get the first 463715 targets
+#y <- data[1:324600, 1]
+
+#m <- length(y)
+
+# Starts to normalize the features
+mu <- matrix(0, 1, ncol(X))
+sigma <- matrix(0, 1, ncol(X))
+
+mu <- mu + apply(X, 2, mean)
+sigma <- sigma + apply(X, 2, sd)
+
+X <- X - mu
+X <- X / sigma
+
+# Grid
+#grid <- seq(0.06, 0.07, 0.002)
+#grid_length <- length(grid)
+
+alpha = 0.066
+
+# Starts Gradient Descent
+num_iters <- 400
+theta = matrix(0, 90, 1)
+J_history = matrix(0, num_iters, 1)
+X_sum <- apply(X, 2, sum)
+
+#for (i in 1:grid_length) {
+  
+  #alpha <- grid[i]
+  
+  for (i in 1:num_iters) {
+    cat(" Current iteration: ", i)
+    
+    theta_transp <- t(theta)
+    
+    M <- theta_transp * X
+    M_col_sum <- apply(M, 2, sum)
+    S <- (M_col_sum - y) * X
+    #S <- S * X
+    
+    theta <- theta_transp - ((alpha/m) * X_sum)
+    
+    # Calculate Cost Function J
+    #M <- theta_transp * X
+    M_row_sum <- apply(M, 1, sum)
+    S <- (M_row_sum - y)^2
+    #S <- S^2
+    
+    J_history[i] <- (sum(S) / (2*m))
+  } 
+  
+  jpeg(paste(paste(alpha, num_iters, sep="_"), "jpg", sep="."))
+  plot(J_history, type="o", col="blue")
+  dev.off()
+#}
+
+
+time2 <- proc.time()
+
+time2 - time1
