7cd18f771634d38d6e2adc7e7dc4d7d66ab1ae5d
Victor Leal <victorleal18@gmail.com>
2015-09-28 23:36:52 -0300

    Running for each category

--- unicamp/assignment2/assignment2.R
+++ unicamp/assignment2/assignment2.R
@@ -1,7 +1,7 @@
-rm(list=ls())
+#rm(list=ls())
 time1 <- proc.time()
 
-# Load the data
+#Load the data
 df <- read.csv(file.choose(), header=T)
 
 df$datetime <- strptime(x = as.character(df$Dates), format="%Y-%m-%d %H:%M:%S")
@@ -17,19 +17,24 @@ df$wday <- as.POSIXlt(df$datetime)$wday
 
 #http://stackoverflow.com/questions/23103223/converting-factors-to-numeric-values-in-r
 df$PdDistrict.f <- as.numeric(factor(df$PdDistrict , levels(df$PdDistrict)))
-
-df$Category.W <- 0
-df$Category.W[df$Category=='WARRANTS'] <- 1
-
-head(df)
-
 df$PdDistrict <- factor(df$PdDistrict)
-# 
-# 
+
 drops <- c('Dates', 'DayOfWeek', 'Resolution', 'Address', 'Descript', 'datetime')
 data <- df[, !(names(df) %in% drops)]
-# 
-mylogit <- glm(Category.W ~ minute+PdDistrict.f+X+Y, data=data, family = "binomial")
-# 
-summary(mylogit)
+head(data)
+
+categories = levels(df$Category)
+
+for(i in 1:length(categories)){
+  time1 <- proc.time()
+  print(categories[i])
+  data$Category.W <- 0
+  data$Category.W[data$Category==categories[i]] <- 1 
+  
+  mylogit <- glm(Category.W ~ minute+PdDistrict.f+X+Y, data=data, family = "binomial")
+  summary(mylogit)
+  time2 <- proc.time()
+  
+  time2 - time1
+}
 
